70b13ab998dae88b365f5fd5b78b130f1e88a349
Tahirmuris <tahirmuris@gmail.com>
2021-09-01 15:16:49 +0530

    student list print

--- Student-Management-JPA/src/test/java/com/mts/StudentManagementJPA/repositories/Test/StudentRepositoryTest.java
+++ Student-Management-JPA/src/test/java/com/mts/StudentManagementJPA/repositories/Test/StudentRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -26,5 +28,13 @@ class StudentRepositoryTest {
                 build();
 
         studentRepository.save(student);
+    }
+
+    @Test
+    public void printStudent()
+    {
+        List<Student> listStudents = studentRepository.findAll();
+
+        System.out.println("students = "+listStudents);
     }
 }
